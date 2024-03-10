0554b4d8aa8b663223c87402ccf831f2930c4910
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-03-10 16:45:22 -0700

    update actions

--- app/engine/actions.rb
+++ app/engine/actions.rb
@@ -9,57 +9,16 @@ class Action # rubocop:disable Style/Documentation
 
   def current_player
     state.current_player
-  end 
+  end
 
   def execute
     case type
-    when :player_draw
-      current_player.draw(value)
-    when :add_attack
-      current_player.add_attack(value)
+    when :draw, :add_attack, :clank_cube, :coin, :heal
+      current_player.send(type, value)
     when :discard
       current_player.discard_card(value)
-    when :clank_cube
-      current_player.clank_cube(value)
-    when :draw
-      state.draw(value)
-    when :buy_card
-      state.buy_card(value)
-    when :teleport
-      state.teleport(value)
-    when :dragon_cubes
-      state.dragon_cubes(value)
-    when :coin
-      current_player.coin(value)
-    when :heal
-      current_player.heal(value)
-    when :move
-      state.move(value)
-    when :end
-      state.end_turn
-    when :dragon_attack
-      state.dragon_attack
+    when :buy_card, :teleport, :dragon_cubes, :move, :dragon_attack, :end_turn
+      state.public_send(type, value)
     end
+  end
 end
-
-class DrawAction < Action; end
-
-class AttackAction < Action; end
-
-class DiscardAction < Action; end
-
-class TrashAction < Action; end
-
-class TeleportAction < Action; end
-
-class DragonCubesAction < Action; end
-
-class CoinAction < Action; end
-
-class HealAction < Action; end
-
-class MoveAction < Action; end
-
-class ClankAction < Action; end
-
-class EndAction < Action; end
