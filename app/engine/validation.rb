bef0e24e17fbc7c2536eb455cf87ce29c675d315
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-03-11 20:59:18 -0700

    validation card system

--- app/engine/validation.rb
+++ app/engine/validation.rb
@@ -2,7 +2,21 @@
 
 class Validation
   class TypeUnknown < StandardError; end
-
+  # TODO: fix card mess below
+  COMPANION_CARDS = YAML.load_file(Rails.root.join('config', 'game', 'companions.yml'))
+  DEVICE_CARDS = YAML.load_file(Rails.root.join('config', 'game', 'devices.yml'))
+  GEM_CARDS = YAML.load_file(Rails.root.join('config', 'game', 'gems.yml'))
+  ITEM_CARDS = YAML.load_file(Rails.root.join('config', 'game', 'items.yml'))
+  MONSTER_CARDS = YAML.load_file(Rails.root.join('config', 'game', 'monsters.yml'))
+  RESERVE_CARDS = YAML.load_file(Rails.root.join('config', 'game', 'reserves.yml'))
+  STARTING_DECK_CARDS = YAML.load_file(Rails.root.join('config', 'game', 'starting_deck.yml'))
+  CARD_NAMES = COMPANION_CARDS.map { |c| c['name'] } +
+               DEVICE_CARDS.map { |c| c['name'] } +
+               GEM_CARDS.map { |c| c['name'] } +
+               ITEM_CARDS.map { |c| c['name'] } +
+               MONSTER_CARDS.map { |c| c['name'] } +
+               RESERVE_CARDS.map { |c| c['name'] } +
+               STARTING_DECK_CARDS.map { |c| c['name'] }
   attr_reader :gameplay_data
 
   def initialize(gameplay_data)
@@ -15,10 +29,10 @@ class Validation
     klass = case type
             when Executeable::PLAYER_ACTIONS.include?(type)
               Validation::Player
+            when CARD_NAMES.include?(type)
+              Validation::Game
             when Executeable::CARD_ACTIONS.include?(type)
               Validation::Card
-            when Executeable::GAME_ACTIONS.include?(type)
-              Validation::Game
             end
     klass.new(gameplay_data, value:, type:).valid?
   end
--- app/engine/validation/card.rb
+++ app/engine/validation/card.rb
@@ -1,7 +1,36 @@
 # frozen_string_literal: true
 
+# TODO: this file needs some work
 class Validation::Card < Validation::Base
+  LOGIC_OPERATORS = ['==', '!=', '>', '>=', '<', '<='].freeze
+  LOGIC_VALUES = ['true', 'false', /\d+/].freeze
+  VALID_CONDITIONALS = %i[environment].freeze
   def valid?
-    true
+    return true unless any_conditions?
+
+    valid_conditons.all? do |condition|
+      evaluate_position(condition['logic'])
+    end
+  end
+
+  def card
+    @card ||= [COMPANION_CARDS, DEVICE_CARDS, GEM_CARDS, ITEM_CARDS,
+               MONSTER_CARDS, RESERVE_CARDS, STARTING_DECK_CARDS].flatten.find_by do |card_type|
+      return card_type.find { |card| card['name'] == type }
+    end
+  end
+
+  def valid_conditons
+    card.fetch('conditionals', []).filter { |cond| VALID_CONDITIONALS.include?(cond['type']) }
+  end
+
+  def any_conditions?
+    valid_conditons.any?
+  end
+
+  def evaluate_position(logic)
+    logic_key, logic_operator, logic_value = logic.split
+    logic_value = logic_value.match?(/\d+/) ? logic_value.to_i : logic_value.downcase
+    current_player.position.send(logic_key).public_send(logic_operator, logic_value)
   end
 end
