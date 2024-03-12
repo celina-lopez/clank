1cafcff5c14b9cbb9c094122673feddcaa4ccb76
Celina <57647158+celina-lopez@users.noreply.github.com>
2024-03-12 13:26:47 +0900

    Update base.rb

--- app/engine/action/base.rb
+++ app/engine/action/base.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class Action::Base
+  attr_accesor :gameplay_data
+
+  def initialize(gameplay_data, type:, value:)
+    @gameplay_data = gameplay_data
+    @type = type 
+    @value = value
+  end
+
+  def execute!
+    send(type, value)
+  end
 end
