a3fa659d08313d3462f3feceba6d89423e6ec928
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-03-13 18:58:38 -0700

    create game model

---
+++ db/migrate/20240314015732_create_games.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class CreateGames < ActiveRecord::Migration[7.0]
+  def change
+    create_table :games do |t|
+      t.json :data, default: {}, null: false
+      t.timestamps
+    end
+  end
+end
