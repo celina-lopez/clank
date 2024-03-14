95d56c9794c09d5414de302d2a89b7be5df7d547
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-03-13 19:00:12 -0700

    annotate

--- test/factories/games.rb
+++ test/factories/games.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: games
+#
+#  id         :integer          not null, primary key
+#  data       :json             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :game do
     
