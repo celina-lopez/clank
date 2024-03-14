abcd0ad25377a38dbcc1cbe4bd170b77f76bde1a
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-03-13 18:40:34 -0700

    base engine fix

--- lib/test.rb
+++ lib/test.rb
@@ -3,9 +3,6 @@ new_engine.execute(type: 'start_game', value: '3')
 new_engine.gameplay_data.current_player.deck.active.map { |card| card['name'] }.each do |type|
   new_engine.execute(type:, value: '3')
 end
-new_engine.execute(type: 'end_turn', value: '3')
-new_engine.gameplay_data.current_player.deck.active.map { |card| card['name'] }.each do |type|
-  new_engine.execute(type:, value: '3')
-end
-# new_engine.execute(type: 'move', value: '2')
 player = new_engine.gameplay_data.current_player
+new_engine.execute(type: 'move', value: '1') if player.move_points > 0
+new_engine.execute(type: 'end_turn', value: '3')
