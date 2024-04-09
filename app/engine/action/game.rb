56550462f8a840e92f1d4fa432883670885b6f69
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-04-09 15:55:55 -0700

    calculate victory points

--- app/engine/action/game.rb
+++ app/engine/action/game.rb
@@ -3,10 +3,10 @@
 class Action::Game < Action::Base
   def end_turn
     gameplay_data.next_player!
+    return end_game! if end_game?
+
     drawn_cards = gameplay_data.deck.reload_active_deck
     fullfill_immediate_actions(drawn_cards)
-    if end_game?
-    end
   end
 
   def start_game
@@ -15,8 +15,14 @@ class Action::Game < Action::Base
 
   private
 
+  def end_game!
+    CalculateVictoryPoints.new(gameplay_data).execute!
+  end
+
   def end_game?
+    false
     # player has certain position metadata
+    # gameplay_data.current_player.position == [0, 0]
   end
 
   def fullfill_immediate_actions(newly_drawn_cards)
