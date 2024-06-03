0377b389df30c280056f9b193f8eaff06abaacd7
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-06-03 04:03:00 -0700

    remove some todos

--- app/engine/action/player.rb
+++ app/engine/action/player.rb
@@ -73,7 +73,7 @@ class Action::Player < Action::Base
 
   def card_on_acquire(card)
     card.fetch('acquire', {}).each do |action_type, action_value|
-      history << { type: action_type, value: action_value }
+      history << { type: action_type, value: action_value, player_index: current_player.index }
       redeem_action_on_card(action_type, action_value)
     end
   end
