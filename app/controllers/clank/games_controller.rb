ed092599251c0dc522677d1434051ce0037a22b5
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-07-12 15:43:12 -0700

    picking it up

--- app/controllers/clank/games_controller.rb
+++ app/controllers/clank/games_controller.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class Clank::GamesController < GamesController
-  def new
-    # TODO: add game type, aka Clank!
-    @game = Game.new
-  end
-
   def show
     # TODO: specific game map
     @game = Game.find(params[:id])
