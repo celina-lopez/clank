9dad0b169e46337460bf046844e24eeceb20c63e
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-11-28 17:12:26 -0800

    yassss

--- app/javascript/channels/love_letter_game_helpers.js
+++ app/javascript/channels/love_letter_game_helpers.js
@@ -4,7 +4,14 @@ import { updateBanner, updateLogs } from './utils'
 import { updateStatsForPlayer } from './player_helpers'
 
 // CARD FUNCTIONS
-const statKeys = ['favors'];
+const statKeys = ['victory_points'],
+      rewardToStat = {
+        trade_card: 'trade_card_points',
+        choose_player_to_compare: 'choose_player_to_compare_points',
+        choose_player_to_reveal: 'choose_player_to_reveal_card',
+        choose_player_to_discard: 'choose_player_to_discard_points',
+        choose_player_to_guess: 'choose_player_to_guess_card'
+      };
 
 
 function populateCards(prefix, cards, playerHand=false) {
@@ -26,9 +33,21 @@ function updateStats(game) {
   });
 }
 
+function exposeRewards(player) {
+  Object.keys(rewardToStat).forEach(function(reward_name) {
+    let element = document.getElementById(reward_name);
+    if (player[rewardToStat[reward_name]] > 0) {
+      element.classList.remove('hidden');
+    } else {
+      element.classList.add('hidden');
+    }
+  });
+}
+
 export function updatePlayerData(player, playerId, data) {
   populateCards('player', player['deck']['active'], true);
-  updateBanner(data, playerId)
+  updateBanner(data, playerId);
+  exposeRewards(player);
 }
 // GAME FUNCTIONS
 
