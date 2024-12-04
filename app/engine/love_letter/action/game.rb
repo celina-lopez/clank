a41634eb4c1ae0dec4252b7b91c5e68a2286ab23
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-12-03 17:56:29 -0800

    its a bit complex...

--- app/engine/love_letter/action/game.rb
+++ app/engine/love_letter/action/game.rb
@@ -1,4 +1,4 @@
-# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/AbcSize, Metrics/MethodLength
 # frozen_string_literal: true
 
 class LoveLetter::Action::Game < Action::Game
@@ -7,10 +7,7 @@ class LoveLetter::Action::Game < Action::Game
   end
 
   def end_turn
-    if end_of_round?
-      end_of_round!
-      return
-    end
+    end_of_round! if end_of_round?
     if end_game?
       gameplay_data.end_game = true
       return
@@ -32,18 +29,36 @@ class LoveLetter::Action::Game < Action::Game
     false
   end
 
+  def distribute_favor_tokens_with_empty_deck # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    highest_score = 0
+    spy_players = []
+    valid_players = gameplay_data.players.filter do |player|
+      valid = !player.removed_from_round
+      if valid
+        highest_score = [player.deck.active.first['victory_points'], highest_score].max
+        spy_players << player.index if player.deck.discarded.find { |card| card['name'] == 'spy' }
+      end
+      valid
+    end
+    valid_players.each do |player|
+      player.favor_tokens += 1 if player.deck.active.first['victory_points'] == highest_score
+      player.favor_tokens += 1 if spy_players.one? && spy_players.include?(player.index)
+    end
+  end
+
   def end_of_round!
     if gameplay_data.deck.deck.empty?
-      valid_players = gameplay_data.players.filter { |player| !player.removed_from_round }
-      highest_score = valid_players.map { |player| player.deck.active.first['victory_points'] }.max
-      valid_players.each do |player|
-        player.favor_tokens += 1 if player.deck.active.first['victory_points'] == highest_score
+      distribute_favor_tokens_with_empty_deck
+    else
+      gameplay_data.players.each do |player|
+        player.favor_tokens += 1 unless player.removed_from_round
       end
     end
     gameplay_data.deck = LoveLetter::Model::Deck.new(LoveLetter::Base::CARDS, num_of_active_cards: 0)
     gameplay_data.setup_deck(gameplay_data.players.size)
     gameplay_data.players.each do |player|
       player.deck = LoveLetter::Model::Deck.new(active: gameplay_data.deck.deck.pop(1))
+      player.removed_from_round = false
     end
   end
 
@@ -54,4 +69,4 @@ class LoveLetter::Action::Game < Action::Game
   end
 end
 
-# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/AbcSize, Metrics/MethodLength
