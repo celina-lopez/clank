1e052dabecfc1bb57d9be561cb2147a0a5cefd9d
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-12-12 10:42:18 -0800

    name generator for love letter

--- app/services/random_name_generator.rb
+++ app/services/random_name_generator.rb
@@ -3,8 +3,11 @@
 class RandomNameGenerator
   GAME_ADJECTIVES = %w[Legendary Epic Cunning Stealthy Fierce Enchanted Heroic Mystical Relentless Swift].freeze
   GAME_NOUNS = %w[Dragons Knights Mages Rogues Goblins Trolls Wizards Rangers Clerics Barbarians].freeze
-  NAME_FIRST = %w[Ashen Thunder Ember Frost Shadow Iron Storm Blaze Silver Dark Wild].freeze
-  NAME_SECOND = %w[blade heart shade song claw bane fist stone seeker whisper forge].freeze
+  FANTASY_NAME_FIRST = %w[Ashen Thunder Ember Frost Shadow Iron Storm Blaze Silver Dark Wild].freeze
+  FANTASY_NAME_SECOND = %w[blade heart shade song claw bane fist stone seeker whisper forge].freeze
+  ROYAL_NAME_FIRST = %w[King Queen Prince Princess Duke Earl Baron].freeze
+  ROYAL_NAME_SECOND = %w[hearts shades songs whispers dreams stars secrets echoes frost embers twilighyt silence flames
+                         shadows winds mirrors tides chains light ashes].freeze
 
   def self.game_name
     adjective = GAME_ADJECTIVES.sample
@@ -12,9 +15,14 @@ class RandomNameGenerator
     "#{adjective} #{noun}"
   end
 
-  def self.character_name
-    first = NAME_FIRST.sample
-    second = NAME_SECOND.sample
+  def self.character_name(theme = 'fantasy')
+    if theme == 'fantasy'
+      first = FANTASY_NAME_FIRST.sample
+      second = FANTASY_NAME_SECOND.sample
+    elsif theme == 'royal'
+      first = "#{ROYAL_NAME_FIRST.sample} of "
+      second = ROYAL_NAME_SECOND.sample
+    end
     "#{first}#{second}"
   end
 end
