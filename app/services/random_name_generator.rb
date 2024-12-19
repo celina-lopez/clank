da9cf4a6ad6edaa04b9a1042c8edee49dcecd28c
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-12-19 15:16:59 -0800

    um

--- app/services/random_name_generator.rb
+++ app/services/random_name_generator.rb
@@ -3,7 +3,8 @@
 class RandomNameGenerator
   GAME_ADJECTIVES = %w[Legendary Epic Cunning Stealthy Fierce Enchanted Heroic Mystical Relentless Swift].freeze
   GAME_NOUNS = %w[Dragons Knights Mages Rogues Goblins Trolls Wizards Rangers Clerics Barbarians].freeze
-  FANTASY_NAME_FIRST = %w[Ashen Thunder Ember Frost Shadow Iron Storm Blaze Silver Dark Wild].freeze
+  FANTASY_NAME_FIRST = ['Ashen of ', 'Thunder of ', 'Ember of ', 'Frost of ', 'Shadow of ', 'Iron of ', 'Storm of ',
+                        'Blaze of ', 'Silver of ', 'Dark of ', 'Wild of '].freeze
   FANTASY_NAME_SECOND = %w[blade heart shade song claw bane fist stone seeker whisper forge].freeze
   ROYAL_NAME_FIRST = %w[King Queen Prince Princess Duke Earl Baron].freeze
   ROYAL_NAME_SECOND = %w[hearts shades songs whispers dreams stars secrets echoes frost embers twilighyt silence flames
@@ -20,7 +21,7 @@ class RandomNameGenerator
       first = FANTASY_NAME_FIRST.sample
       second = FANTASY_NAME_SECOND.sample
     elsif theme == 'royal'
-      first = "#{ROYAL_NAME_FIRST.sample} of "
+      first = ROYAL_NAME_FIRST.sample
       second = ROYAL_NAME_SECOND.sample
     end
     "#{first}#{second}"
