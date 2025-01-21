6b4876f16c9d0ea5724a689d19f47196ef48888d
Celina Lopez <marisa.celina.lopez4@gmail.com>
2025-01-21 15:25:39 -0800

    maybe this

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [/worthyofapenny/, /63vrm.hatchboxapp/, /playboard.games/]
+  config.action_cable.allowed_request_origins = [/worthyofapenny/, /63vrm.hatchboxapp/, /playboard.games/,
+                                                 'playboard.games']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = false
